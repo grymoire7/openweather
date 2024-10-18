41905ab9137245f4b0036418c50df78331f29356
Tracy Atteberry <tracy@magicbydesign.com>
2024-10-17 22:18:57 -0500

    Update credentials structure

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   # https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
   # http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
   #
-  OWM_API_KEY=Rails.application.credentials.dig(:openweathermap, :api_key)
+  OWM_API_KEY=Rails.application.credentials.dig(Rails.env.to_sym, :openweathermap, :api_key)
 
   def index
     zip_code = '68502'
