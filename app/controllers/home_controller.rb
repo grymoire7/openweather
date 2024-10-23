18238dd46bcb0393b6e05d297e4848d6f0ea0787
Tracy Atteberry <tracy@magicbydesign.com>
2024-10-22 20:05:05 -0500

    Remove unneeded weather model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,23 @@
 # This a thin controller for the home page.
-# It uses the Weather model to fetch weather data for a given address.
+# It uses the WeatherService to fetch weather data for a given address.
 #
-# see models/weather.rb
+# see WeatherService
 
 class HomeController < ApplicationController
   # Our default address is Cupertino, a common default for mac users.
   DEFAULT_ADDRESS = "1 Infinite Loop, Cupertino, CA 95014"
 
+  # GET /
+  # The home page.
   def index
-    @weather = Weather.new(address: address)
+    @weather = WeatherService.call(address: address)
   rescue StandardError => e
     flash.now[:alert] = e.to_s
   end
 
   private
 
+  # @return [String] the address for which to fetch weather data
   def address
     @address = params[:address] || DEFAULT_ADDRESS
   end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -9,66 +9,71 @@ class WeatherService
   OWM_API_KEY=Rails.application.credentials.dig(:development, :openweathermap, :api_key)
   WEATHER_BASE_URL = "https://api.openweathermap.org/data/3.0/onecall"
   CURRENT_ICON_BASE_URL = "http://openweathermap.org/img/wn"
+  TEMP_UNITS = "imperial"
 
-  # @param zip_code [String] the target zip code for weather data
-  # @param units [String] one of "imperial", "standard", or "metric"
-  def initialize(address:, units: "imperial")
-    @address = address
-    @units = units
-  end
-
-  def call
-    weather
-  end
-
-  def self.call(address:, units: "imperial")
-    weather = new(address: address, units: units)
-    weather.call
-  end
-
-  private
-
-  attr_reader :units, :zip_code, :geocoding, :address
-
-  def weather
-    @geocoding = GeocodingService.call(@address)
-    zip = geocoding[:postal_code]
+  # Fetches weather data for a given address.
+  #
+  # @param address [String] the target address for weather data
+  # @return [Hash] the weather data for the address
+  #
+  # @see GeocodingService
+  def self.call(address:)
+    geocoding_data = GeocodingService.call(address)
+    zip = geocoding_data[:postal_code]
 
     results_cached = true
     results = Rails.cache.fetch("weather/zip/#{zip}", exprires_in: 30.minutes, skip_nil: true) do
       results_cached = false
-      combined_results
+      weather_data = fetch_weather_data(geocoding_data[:longitude], geocoding_data[:latitude])
+      selected_results(weather_data, geocoding_data)
     end
     results.merge({ results_cached: results_cached }).with_indifferent_access
   end
 
-  def combined_results
-    response = weather_data_response
-    raise IOError.new("Weather service error: #{response.return_message}") if response.code != 200
+  private
 
-    weather_data = JSON.parse(response.body)
-    weather_data[:geo] = geocoding
-    weather_data[:icon_url] = current_weather_icon(weather_data)
+  # Combines selected weather and geocoding data into a single hash.
+  #
+  # @params: weather_data [Hash] the weather data from the OpenWeatherMap API
+  # @params: geocoding_data [Hash] the geocoding data from the GeocodingService
+  # @returns: [Hash] selected data from the weather and geocoding data
+  def self.selected_results(weather_data, geocoding_data)
+    current_weather = weather_data[:current][:weather].first
+    icon_base = current_weather[:icon]
 
-    weather_data
+    {
+      icon_url:          "#{CURRENT_ICON_BASE_URL}/#{icon_base}@2x.png",
+      status:            current_weather[:main],
+      description:       current_weather[:description],
+      temperature:       weather_data.dig(:current, :temp),
+      high:              weather_data[:daily].first[:temp][:max],
+      low:               weather_data[:daily].first[:temp][:min],
+      feels_like:        weather_data.dig(:current, :feels_like),
+      humidity:          weather_data.dig(:current, :humidity),
+      name:              geocoding_data[:name],
+      latitude:          geocoding_data[:latitude],
+      longitude:         geocoding_data[:longitude],
+      weather_cached:    weather_data[:results_cached],
+      geocoding_cached:  geocoding_data[:results_cached]
+    }
   end
 
-  def current_weather_icon(weather_data)
-    current_weather = weather_data["current"]["weather"].first
-    "#{CURRENT_ICON_BASE_URL}/#{current_weather['icon']}@2x.png"
-  end
-
-  def weather_data_response
-    lon = geocoding[:longitude]
-    lat = geocoding[:latitude]
+  # Fetches weather data from the OpenWeatherMap API.
+  #
+  # @params: lon [Float] the longitude of the target location
+  # @params: lat [Float] the latitude of the target location
+  # @returns: [Hash] the weather data from the OpenWeatherMap API
+  def self.fetch_weather_data(lon, lat)
     exclude = "minutely,hourly"
 
-    # https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
     request = Typhoeus::Request.new(
       WEATHER_BASE_URL,
       method: :get,
-      params: { lat: lat, lon: lon, units: units, exclude: exclude, appid: OWM_API_KEY }
+      params: { lat: lat, lon: lon, units: TEMP_UNITS, exclude: exclude, appid: OWM_API_KEY }
     )
-    request.run
+    response = request.run
+    raise IOError.new("Weather service error: #{response.return_message}") if response.code != 200
+
+    JSON.parse(response.body, symbolize_names: true).with_indifferent_access
   end
 end
